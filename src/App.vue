d85dab93372f05f5f64958f4de9053942680e7aa
jasmynej14 <jasmynej14@brandeis.edu>
2021-05-06 22:05:04 -0400

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <v-app>
-    
+    <v-app-bar color="#ecf0f1" dense>
+      <v-app-bar-title>Political Tinder</v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer class="red lighten-1" expand-on-hover permanent>
+      <v-list dense rounded>
+        
+      </v-list>
+    </v-navigation-drawer>
+    <v-bottom-navigation></v-bottom-navigation>
   </v-app>
 </template>
 
