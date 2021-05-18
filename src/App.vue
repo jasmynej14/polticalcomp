cef96b57bec8636da4d49df9ba10cd6488242b3d
jasmynej14 <jasmynej14@brandeis.edu>
2021-05-17 20:20:17 -0400

    made navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,36 @@
 <template>
   <v-app>
     <v-app-bar color="#ecf0f1" dense>
-      <v-app-bar-title>Political Tinder</v-app-bar-title>
+      <v-app-bar-title>Political Compatibility</v-app-bar-title>
     </v-app-bar>
-    <v-navigation-drawer class="red lighten-1" expand-on-hover permanent>
-      <v-list dense rounded>
-        
-      </v-list>
-    </v-navigation-drawer>
-    <v-bottom-navigation></v-bottom-navigation>
+    <navigation></navigation>
   </v-app>
 </template>
 
 <script>
 
 
+import Navigation from './components/Navigation.vue';
 export default {
+  
   name: 'App',
 
   components: {
-    
+    Navigation
   },
 
-  data: () => ({
-    //
-  }),
+  data () {
+
+      return {
+        items: [
+          { title: 'Home', icon: 'mdi-home' },
+          { title: 'Profile', icon: 'mdi-view-dashboard' },
+          { title: 'Take the Test', icon: 'mdi-newspaper' },
+          { title: 'Map', icon: 'mdi-google-maps' },
+        ],
+        right: null,
+      }
+    }
+  
 };
 </script>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-app>
+        <v-navigation-drawer  permanent>
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+   
+    <v-bottom-navigation></v-bottom-navigation>
+    </v-app>
+</template>
+
+<script>
+    export default {
+  name: 'Navigation',
+
+  components: {
+    
+  },
+
+  data () {
+      return {
+        items: [
+          { title: 'Home', icon: 'mdi-home' },
+          { title: 'Profile', icon: 'mdi-view-dashboard' },
+          { title: 'Take the Test', icon: 'mdi-newspaper' },
+          { title: 'Map', icon: 'mdi-google-maps' },
+        ],
+        right: null,
+      }
+    }
+  
+};
+</script>
